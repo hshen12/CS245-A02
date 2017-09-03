a6281f501a015d17e3e5027cdff73f1a4a4ed05e
hshen12 <hshen12@dons.usfca.edu>
2017-09-02 17:56:33 -0700

    Add files via upload
    please review @toropeza @shayromayo

---
+++ LinearSearch.java
@@ -0,0 +1,26 @@
+package edu.usfca.cs.cs245;
+
+public class LinearSearch implements Practice2Search {
+
+	@Override
+	public String searchName() {
+		
+		
+		
+
+		return "this is a linear search";
+	}
+
+	@Override
+	public int search(int[] arr, int target) {
+
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == target) {
+				return i;
+			}
+		}
+		return -1;
+
+	}
+
+}
